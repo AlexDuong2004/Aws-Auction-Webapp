d237ce813730acf330e632c5503cbeb6835f3af4
Alex <alex.duong@uconn.edu>
2025-02-05 17:48:30 -0500

    Finished lbs->kg tests

--- hw01/test_convert.py
+++ hw01/test_convert.py
@@ -14,3 +14,16 @@ def test_kg_to_lbs():
     assert kg_to_lbs("1000", "kg") == "2204.62 lbs"
     assert kg_to_lbs("32193812948", "kg") == "70975123901.42 lbs"
     assert kg_to_lbs("0", "kg") == "0.00 lbs"
+
+def test_lbs_to_kg():
+    "Ten unit tests for the lbs_to_kg function"
+    assert lbs_to_kg("true", "kg") == 'Error'
+    assert lbs_to_kg("5.2.1", "lbs") == 'Error'
+    assert lbs_to_kg("lbs", "1") == 'Error'
+    assert lbs_to_kg("2", "lb") == 'Error'
+    assert lbs_to_kg(".3.1", "lmbs") == 'Error'
+    assert lbs_to_kg("3.31", "lbs") == "1.50 kg"
+    assert lbs_to_kg("0.77", "lbs") == "0.35 kg"
+    assert lbs_to_kg("2204.62", "lbs") == "1000.00 kg"
+    assert lbs_to_kg("70975123901.42", "lbs") == "32193812948.00 kg"
+    assert lbs_to_kg("0", "lbs") == "0.00 kg"
