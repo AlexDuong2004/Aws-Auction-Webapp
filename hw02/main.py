f08545be52b8f6bd2f1135f7421c9a205626d597
Alex <alex.duong@uconn.edu>
2025-02-12 14:47:58 -0500

    Added templates from tutorial

--- hw02/main.py
+++ hw02/main.py
@@ -2,4 +2,17 @@ import logging
 from fileinput import filename 
 from flask import *  
 app = Flask(__name__)
-app = Flask(__name__)
+
+@app.route('/')   
+def main():   
+    return render_template("upload-button.html")   
+  
+@app.route('/success', methods = ['POST'])   
+def success():   
+    if request.method == 'POST':   
+        f = request.files['file'] 
+        f.save(f.filename)   
+        return render_template("success.html", name = f.filename)   
+  
+if __name__ == '__main__':   
+    app.run(debug=True)
