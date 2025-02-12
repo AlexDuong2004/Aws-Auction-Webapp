7e3a13e96ee5dd59bafcabb2662627277dca25a4
Alex <alex.duong@uconn.edu>
2025-02-12 15:18:07 -0500

    Placed uploaded files into a folder

--- hw02/main.py
+++ hw02/main.py
@@ -1,8 +1,10 @@
 import logging
+import os
 from fileinput import filename 
 from flask import *  
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploads'
 @app.route('/')   
 def main():   
     return render_template("upload-button.html")   
@@ -11,7 +13,8 @@ def main():
 def success():   
     if request.method == 'POST':   
         f = request.files['file'] 
-        f.save(f.filename)   
+        file_path = os.path.join(UPLOAD_FOLDER, f.filename)
+        f.save(file_path)   
         return render_template("success.html", name = f.filename)   
   
 if __name__ == '__main__':   
