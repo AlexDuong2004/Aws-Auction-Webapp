ae52fa94fb4e58a0be374e961db5847c3dd9adaa
Alex <alex.duong@uconn.edu>
2025-03-05 16:55:32 -0500

    Testing upload output

--- hw05/upload.py
+++ hw05/upload.py
@@ -1,10 +1,15 @@
 import json
+import base64
+import boto3
 
 print("Loading Function In")
 
 def my_function3(event, context):
     # TODO implement
     print(event)
+    file_content = base64.b64decode(event['body-json']['body'])
+    print(file_content)
+    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
