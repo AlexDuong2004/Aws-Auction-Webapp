30055649609a5439986ff5ca25adf362590f5456
Alex <alex.duong@uconn.edu>
2025-03-05 15:34:13 -0500

    Wrote the first get command

--- hw05/List_buckets.py
+++ hw05/List_buckets.py
@@ -1,11 +1,18 @@
 import json
+import boto3
 
 print("Loading Function In")
 
 def my_function2(event, context):
     # TODO implement
     print(event)
+    client = boto3.client("s3")
+    information = client.list_buckets()
+    bucket_names = []
+    for i in information.get("Buckets", []):
+        bucket_names.append(i["Name"])
+        
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+            "statusCode": 200,
+            "body": json.dumps({"buckets": bucket_names})
+        }
