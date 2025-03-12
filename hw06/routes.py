c713bfb63d1428735772462920307f698de8be45
Alex <alex.duong@uconn.edu>
2025-03-12 17:15:11 -0400

    Added print statements for logs later

--- hw06/routes.py
+++ hw06/routes.py
@@ -13,11 +13,13 @@ def configure_routes(app):
     @app.route('/')
     def homepage():
         '''Show the main screen. This is the main entry point for the webapp'''
+        print("Booting up the homepage")
         return render_template("index.html")
 
     @app.route('/list_objects')
     def list_files():
         '''Show the main screen'''
+        print("attemptiing to show list of objects")
         return resource_s3.list_objects(BUCKET_NAME)
 
     @app.route('/upload', methods = ['POST'])
@@ -26,6 +28,7 @@ def configure_routes(app):
         if request.method == 'POST':
             file = request.files['file']
             resource_s3.upload_file_to_s3(file, BUCKET_NAME)
+            print("File successfully uploaded")
             return list_files()
         return "Operation not supported"
     
@@ -38,8 +41,9 @@ def configure_routes(app):
             return jsonify({'error': 'Missing object key'}), 400
         thumbnail = resource_s3.generate_thumbnail(BUCKET_NAME, obj_key)
     
-        if thumbnail:
-            return Response(thumbnail, content_type='image/jpeg')  # Change content type if needed
+        if thumbnail:   
+            print("successfully got the thumbnail")
+            return Response(thumbnail, content_type='image/jpeg')  
         else:
             return jsonify({'error': 'Object not found'}), 404
     
